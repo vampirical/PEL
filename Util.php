bbe171462577b9fe235a71a5d6fbeb55fd8169c4
Jeff Marion <marion.jeff@gmail.com>
2011-03-17 05:08:30 -0700

    Adding utf16 helper function

--- Util.php
+++ Util.php
@@ -105,6 +105,38 @@ class Util
 
 		return $sxmlBase;
 	}
+
+	public static function utf16ToUtf8($str) {
+		$c0 = ord($str[0]);
+		$c1 = ord($str[1]);
+
+		if ($c0 == 0xFE && $c1 == 0xFF) {
+			$be = true;
+		} else if ($c0 == 0xFF && $c1 == 0xFE) {
+			$be = false;
+		} else {
+			return $str;
+		}
+
+		$str = substr($str, 2);
+		$len = strlen($str);
+		$dec = '';
+		for ($i = 0; $i < $len; $i += 2) {
+			$c = ($be) ? ord($str[$i]) << 8 | ord($str[$i + 1]) : 
+			ord($str[$i + 1]) << 8 | ord($str[$i]);
+			if ($c >= 0x0001 && $c <= 0x007F) {
+				$dec .= chr($c);
+			} else if ($c > 0x07FF) {
+				$dec .= chr(0xE0 | (($c >> 12) & 0x0F));
+				$dec .= chr(0x80 | (($c >>  6) & 0x3F));
+				$dec .= chr(0x80 | (($c >>  0) & 0x3F));
+			} else {
+				$dec .= chr(0xC0 | (($c >>  6) & 0x1F));
+				$dec .= chr(0x80 | (($c >>  0) & 0x3F));
+			}
+		}
+		return $dec;
+	}
 }
 
 ?>
